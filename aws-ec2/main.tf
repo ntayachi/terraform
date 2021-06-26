f0d55150116b681143d4090946afa9ecbff6b3a0
ntayachi <tayachi.nafaa@gmail.com>
2021-06-26 20:47:39 +0100

    add 2 ebs volmues and attach them using for_each

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -2,13 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  volumes = toset([
+    "xvdf",
+    "xvdg"
+  ])
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["self"]
 
   filter {
     name   = "name"
-    values = ["Amazon Linux 2 latest*"]
+    values = ["Amazon Linux*"]
   }
 
   filter {
@@ -20,4 +27,19 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+}
+
+resource "aws_ebs_volume" "ebs_volumes" {
+  for_each = local.volumes
+  
+  availability_zone = var.ebs_volume_az
+  size              = 20
+}
+
+resource "aws_volume_attachment" "name" {
+  for_each = local.volumes
+
+  device_name = "/dev/${each.key}"
+  volume_id   = aws_ebs_volume.ebs_volumes["${each.key}"].id
+  instance_id = aws_instance.web_server.id
 }
--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "Region where to provision resources"
 }
+
+variable "ebs_volume_az" {
+  type = string
+  default = "us-east-1a"
+  description = "Availability Zone for the EBS volumes"
+}
