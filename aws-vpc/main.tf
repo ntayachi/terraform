2108326033b3a84ff1d8ff42f4da5025f81eee13
ntayachi <tayachi.nafaa@gmail.com>
2021-07-23 23:07:48 +0100

    create internet GW

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -30,3 +30,11 @@ resource "aws_subnet" "myPrivateSubnet" {
     "Name" = "myPrivateSubnet"
   }
 }
+
+resource "aws_internet_gateway" "myIGW" {
+  vpc_id = aws_vpc.myVPC.id
+
+  tags = {
+    "Name" = "myIGW"
+  }
+}
