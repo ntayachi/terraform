a27773663fde918b485287df4f70a5f372af7050
ntayachi <tayachi.nafaa@gmail.com>
2021-07-23 22:43:51 +0100

    create public and private subnets

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -4,7 +4,29 @@ provider "aws" {
 
 resource "aws_vpc" "myVPC" {
   cidr_block = "10.0.0.0/16"
+  
   tags = {
     "Name" = "myVPC"
+  }
+}
+
+resource "aws_subnet" "myPublicSubnet" {
+  vpc_id = aws_vpc.myVPC.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch =  true
+
+  tags = {
+    "Name" = "myPublicSubnet"
+  }
+}
+
+resource "aws_subnet" "myPrivateSubnet" {
+  vpc_id = aws_vpc.myVPC.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  
+  tags = {
+    "Name" = "myPrivateSubnet"
   }
 }
