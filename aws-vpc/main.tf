f42a4be59a94668208e3535caa9e514518c212a7
ntayachi <tayachi.nafaa@gmail.com>
2021-07-23 22:45:12 +0100

    fix idendation

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -11,9 +11,9 @@ resource "aws_vpc" "myVPC" {
 }
 
 resource "aws_subnet" "myPublicSubnet" {
-  vpc_id = aws_vpc.myVPC.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.myVPC.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch =  true
 
   tags = {
@@ -22,8 +22,8 @@ resource "aws_subnet" "myPublicSubnet" {
 }
 
 resource "aws_subnet" "myPrivateSubnet" {
-  vpc_id = aws_vpc.myVPC.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.myVPC.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   
   tags = {
