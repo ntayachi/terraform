7408802520757c5a2ac4822e70106a53b2e834e4
ntayachi <tayachi.nafaa@gmail.com>
2021-07-24 09:22:48 +0100

    associate public subnet with the public route table

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -48,7 +48,12 @@ resource "aws_route_table" "myPublicRouteTable" {
 }
 
 resource "aws_route" "internetRoute" {
-  route_table_id = aws_route_table.myPublicRouteTable.id
+  route_table_id         = aws_route_table.myPublicRouteTable.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.myIGW.id
+  gateway_id             = aws_internet_gateway.myIGW.id
+}
+
+resource "aws_route_table_association" "publicSubnetAssociation" {
+  subnet_id      = aws_subnet.myPublicSubnet.id
+  route_table_id = aws_route_table.myPublicRouteTable.id
 }
